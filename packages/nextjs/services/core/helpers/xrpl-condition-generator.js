44c610130fb06fd59b080daf8d732abbd0fd5e20
“Oscar <edsphinx@gmail.com>
2025-08-03 09:32:03 -0600

    fix: refactored script of secret generator to be compatible with serverless infra

--- packages/nextjs/services/core/helpers/xrpl-condition-generator.js
+++ packages/nextjs/services/core/helpers/xrpl-condition-generator.js
@@ -7,23 +7,26 @@ const crypto = require("crypto");
 // This script is the source of truth for the secret (preimage).
 // It always generates a new secret, saves it to a file,
 // and outputs a JSON object with the condition and fulfillment.
-const secretBuffer = crypto.randomBytes(32);
-const secretHex = secretBuffer.toString("hex").toUpperCase();
+function generateConditionData() {
+  const secretBuffer = crypto.randomBytes(32);
+  const secretHex = secretBuffer.toString("hex").toUpperCase();
 
-// fs.writeFileSync("./preimageData.txt", secretHex);
+  // fs.writeFileSync("./preimageData.txt", secretHex);
 
-const fulfillment = new cc.PreimageSha256();
-fulfillment.setPreimage(secretBuffer);
+  const fulfillment = new cc.PreimageSha256();
+  fulfillment.setPreimage(secretBuffer);
 
-const condition = fulfillment.getConditionBinary().toString("hex").toUpperCase();
-const fulfillmentHex = fulfillment.serializeBinary().toString("hex").toUpperCase();
-const hashlock = crypto.createHash("sha256").update(secretBuffer).digest("hex").toUpperCase();
+  const condition = fulfillment.getConditionBinary().toString("hex").toUpperCase();
+  const fulfillmentHex = fulfillment.serializeBinary().toString("hex").toUpperCase();
+  const hashlock = crypto.createHash("sha256").update(secretBuffer).digest("hex").toUpperCase();
 
-const output = {
-  secret: secretHex,
-  hashlock: "0x" + hashlock,
-  condition: condition,
-  fulfillment: fulfillmentHex,
-};
+  const output = {
+    secret: secretHex,
+    hashlock: "0x" + hashlock,
+    condition: condition,
+    fulfillment: fulfillmentHex,
+  };
 
-console.log(JSON.stringify(output, null, 2));
+  return output;
+}
+module.exports = generateConditionData;
--- packages/nextjs/services/core/start/evm-initiator.ts
+++ packages/nextjs/services/core/start/evm-initiator.ts
@@ -1,7 +1,7 @@
 import ESCROW_FACTORY_ABI from "../../../externalAbis/EscrowFactory.json";
 import { packTimelocks } from "../helpers/timelocks-helper";
+import generateConditionData from "../helpers/xrpl-condition-generator";
 import axios from "axios";
-import { execSync } from "child_process";
 import { Wallet, ethers } from "ethers";
 
 // import { config } from "hardhat";
@@ -61,7 +61,7 @@ export async function initiateEvmEscrow(
   try {
     // This script generates the secret, hashlock, condition, and fulfillment.
     // The 'condition' is for XRP Ledger, 'hashlock' for EVM, and 'fulfillment' is the secret itself.
-    conditionData = JSON.parse(execSync("node services/core/helpers/xrpl-condition-generator.js").toString());
+    conditionData = generateConditionData();
     console.log("[DEBUG] Generated condition data:", conditionData);
   } catch (error: any) {
     logProgress(
